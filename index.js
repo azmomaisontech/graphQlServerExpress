b05d48e2b41373a13248950e2a75b303c88f75d3
AZMOMAISON <mosesaizee@outlook.com>
2020-08-04 04:48:13 -0500

    Basic Installation and Starting Server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MODE = process.env.NODE_ENV;
+
+app.listen(PORT, () => {
+  console.log(`
+Server running in ${MODE} Mode, Listening on Port ${PORT}`);
+});
