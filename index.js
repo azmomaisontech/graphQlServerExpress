0ab1d924c695adbc6ffe6799bde7ffd12c5748b1
AZMOMAISON <mosesaizee@outlook.com>
2020-08-04 05:32:24 -0500

    Built the Graphql Basics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const dotenv = require("dotenv");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -10,6 +11,36 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All night coding"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`
