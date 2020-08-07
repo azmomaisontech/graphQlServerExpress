90139fd07017197206699faace5560117538a1c7
AZMOMAISON <mosesaizee@outlook.com>
2020-08-07 04:29:08 -0500

    Added a trycatch to a promise function

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -37,16 +37,16 @@ module.exports = {
   },
 
   createUser: async ({ userInput }) => {
-    const alreadyExist = await User.findOne({ email: userInput.email });
-    if (alreadyExist) throw new Error("User already exists");
-
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(userInput.password, salt);
-    const user = {
-      email: userInput.email,
-      password: password
-    };
     try {
+      const alreadyExist = await User.findOne({ email: userInput.email });
+      if (alreadyExist) throw new Error("User already exists");
+
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(userInput.password, salt);
+      const user = {
+        email: userInput.email,
+        password: password
+      };
       const newUser = new User(user);
       return newUser.save();
     } catch (err) {
