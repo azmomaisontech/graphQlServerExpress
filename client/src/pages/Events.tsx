db6e40ee0aa0b454ed0913be80383fcadf384888
AZMOMAISON <mosesaizee@outlook.com>
2020-09-08 08:17:28 -0500

    Added form to the Event Modal

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import EventModal from "../components/Modal/EventModal";
 import "../pageStyles/Events.css";
 import Backdrop from "../components/Backdrop/Backdrop";
 
 const Events: React.FC = () => {
   const [creating, setCreating] = useState(false);
+
+  const titleRef = useRef<HTMLInputElement | null | undefined>();
+  const priceRef = useRef();
+  const dateRef = useRef();
+  const descriptionRef = useRef<HTMLInputElement>(null);
 
   const openModal = () => {
     setCreating(true);
@@ -23,7 +28,24 @@ const Events: React.FC = () => {
       {creating && <Backdrop />}
       {creating && (
         <EventModal title="New Event" canCancel canConfirm onCancel={handleCancel} onConfirm={handleConfirm}>
-          <p>Modal Content</p>
+          <form>
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" ref={titleRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" id="price" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="date" id="date" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea name="description" id="description" rows={4}></textarea>
+            </div>
+          </form>
         </EventModal>
       )}
       <div className="events-control">
